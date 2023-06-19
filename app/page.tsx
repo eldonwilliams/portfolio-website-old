4cdb78d1f503f4ee22e69665b48777aff4d96dbb
Eldon Williams <eldon.ew.com@gmail.com>
2023-06-19 04:46:52 -0500

    :construction: test Typography

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
+import Typography from "@/components/Typography";
+
 export default function Home() {
     return (
         <>
             <main>
-                
+                <Typography>
+                    <Typography>
+                        Go to&nbsp;
+                    </Typography>
+                    <Typography element="a" href="https://google.com/">
+                        here?
+                    </Typography>
+                </Typography>
             </main>
         </>
     )
